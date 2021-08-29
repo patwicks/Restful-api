3f5add9a3212c2e0bceebe687378ddce3d61a27d
patwicks <it.patrickjohngsantillan@gmail.com>
2021-08-29 10:18:02 +0800

    first commit

--- controller/controller.driver.js
+++ controller/controller.driver.js
@@ -100,7 +100,7 @@ const LOGIN_USER =  async (req, res) => {
     // Password is Correct
     // create and assigned token
     const token = jwt.sign({_id: driver._id }, process.env.TOKEN_SECRET);
-    res.header('authtoken', token).json({userID: driver._id, userToken: token});
+    res.header('authtoken', token).json({userID: driver._id, userType: driver.accountType, userToken: token});
     }catch(err) {
         return res.status(400).json({error: 'Invalid Account!'})
     }  
